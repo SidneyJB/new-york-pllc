67208ca0438a516f9ad61d056ea105fe7f5425b9
Sidney <sidneyjbrodsky@gmail.com>
2025-10-22 15:20:55 -0400

    Add Google Analytics tracking code to layout.tsx
    - Add Google tag (gtag.js) script with account ID AW-17672972971
    - Include gtag configuration for tracking page views and user interactions
    - Script placed in head section to load on all pages

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -99,6 +99,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17672972971"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17672972971');
+            `,
+          }}
+        />
+      </head>
       <body className={`${inter.variable} ${jetbrainsMono.variable} antialiased`}>
         <script async dangerouslySetInnerHTML={{
           __html: `
